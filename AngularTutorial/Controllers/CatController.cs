41d7d5aaf106f545b245739e90cce0b4faba201f
Maziarella <kiebala.magdalena@gmail.com>
2019-02-20 13:33:32 +0100

    API returns process details: start time, threads, processor time, run time, memory usage.

--- AngularTutorial/Controllers/CatController.cs
+++ AngularTutorial/Controllers/CatController.cs
@@ -2,6 +2,7 @@
 using AngularTutorial.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System;
 
 namespace AngularTutorial.Controllers
 {
@@ -23,10 +24,10 @@ namespace AngularTutorial.Controllers
         }
 
         [HttpGet("{id}")]
-        public SimplifiedProcess Get(string id)
+        public DetailedProcess Get(string id)
         {
             Process proccess = Process.GetProcessById(int.Parse(id));
-            return new SimplifiedProcess(proccess);
+            return new DetailedProcess(proccess);
         }
 
         [HttpPost]
@@ -70,12 +71,16 @@ namespace AngularTutorial.Controllers
         public string StartTime { get; set; }
         public string Threads { get; set; }
         public string ProcessorTime { get; set; }
+        public string RunTime { get; set; }
+        public string MemoryUsage { get; set; }
 
         public DetailedProcess(Process process) : base(process)
         {
-            StartTime = process.StartTime.ToString("MM/dd/yyyy HH:mm:ss");
+            StartTime = process.StartTime.ToString("dd/MM/yyyy HH:mm:ss");
             Threads = process.Threads.ToString();
             ProcessorTime = process.TotalProcessorTime.ToString();
+            RunTime = (DateTime.Now - process.StartTime).ToString();
+            MemoryUsage = ((double)process.PrivateMemorySize64/1024/1024).ToString("0.0") + " MB";
         }
 
     }
